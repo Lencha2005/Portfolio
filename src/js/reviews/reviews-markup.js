0d93cdc769b62da49d1bfac8340aa4fc2e96387c
Lena_Bondar <lencha.2005@gmail.com>
2024-09-13 13:55:47 +0300

    update

--- src/js/reviews/reviews-markup.js
+++ src/js/reviews/reviews-markup.js
@@ -9,7 +9,8 @@ return `
                 alt="user-photo"
                 onerror="this.src='/img/reviews/profile-photo.png'"
                 width="48"
-                height="48">
+                height="48"
+                loading="lazy">
                 <h3 class="reviews-title-item">${author}</h3>
                 <p class="reviews-text-item">${review}</p>
             </li>`
